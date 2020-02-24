2138811bfc861a6b06f2e78693ae438b6e81a55b
kevin lautermilch <kevin.laut@outlook.com>
2020-02-24 14:03:36 -0500

    :)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Route, Router } from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
-const App = () => (
+const App = () => {
+  return (
   <div>
     <Navigation />
+    <Route exact path='/' component={Home}/>
+    <Route path='/About' component={About}/>
+    <Route path='/Contact' component={Contact}/>
+    
   </div>
-);
+  )};
 
 export default App;
